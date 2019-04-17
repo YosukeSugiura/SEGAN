e181d0d612474d55d6b45f3a95a297bdb4ee2736
YosukeSUGIURA <44398746+YosukeSugiura@users.noreply.github.com>
2019-04-17 19:01:10 +0900

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,41 @@
+#
+# Settings for SEGAN
+#
+
+class settings:
+
+    def __init__(self):
+
+        # General Settings
+        self.len                = 2 ** 14           # Input Size (len = 16384)
+        self.device_id          = 0                 # GPU ID (init:0)
+
+        # Parameters
+        self.batch_size 	    = 200               # Batch size
+        self.epoch              = 100               # Epoch
+        self.learning_rate_gen  = 0.0001            # Learning Rate (Generator)
+        self.learning_rate_dis  = 0.00001           # Learning Rate (Discriminator)
+        self.weight_decay 	    = 0.0001            # Regularization parameter
+
+        # Retrain
+        self.epoch_from         = 15                 # Epoch No. from that Retraining starts (init:0)
+
+        # Save path
+        self.model_save_path    = 'params'          # Network model path
+        self.model_save_cycle   = 1                 # Epoch cycle for saving model (init:1)
+
+        # Wave files
+        self.clean_train_path   = '../00_data/SEGAN/clean_trainset_wav'     # Folder containing clean wav (train)
+        self.noisy_train_path   = '../00_data/SEGAN/noisy_trainset_wav'     # Folder containing noisy wav (train)
+        self.clean_test_path    = '../00_data/SEGAN/clean_testset_wav'      # Folder containing clean wav (test)
+        self.noisy_test_path    = '../00_data/SEGAN/noisy_testset_wav'      # Folder containing noisy wav (test)
+
+        # Pkl files for train
+        self.train_pkl_path     = 'pkl'             # Folder of pkl files for train
+        self.train_pkl_clean    = 'train_clean.pkl' # File name of "Clean" pkl for train
+        self.train_pkl_noisy    = 'train_noisy.pkl' # File name of "Noisy" pkl for train
+
+        # Pkl files for test
+        self.test_pkl_path      = 'pkl'             # Folder of pkl files for test
+        self.test_pkl_clean     = 'test_clean.pkl'  # File name of "Clean" pkl for test
+        self.test_pkl_noisy     = 'test_noisy.pkl'  # File name of "Noisy" pkl for test
