1a2a391278ae8b0ba7f2db42bfdb7ef4cc5a20fe
YosukeSUGIURA <44398746+YosukeSugiura@users.noreply.github.com>
2019-04-17 19:02:25 +0900

    Update data.py

--- data.py
+++ data.py
@@ -16,7 +16,7 @@ import numpy as np
 import numpy.random as rd
 from scipy.signal import lfilter
 
-from parameters import parameters
+from settings import settings
 
 import os
 
@@ -27,86 +27,101 @@ def de_emph(y, preemph=0.95):
         return y
     return lfilter(1,[1, -preemph], y)
 
-def data_loader(preemph=0.95):
+# Dataset loader
+def data_loader(test=False, preemph=0.95):
     """
-    Read wav files or Load pkl files including wav information
+    Read wav files or Load pkl files
 	"""
 
-	# Parameters 読み取り
-    args = parameters()
+    ## Sub function : wav read & data shaping
+    def wavloader(filename, length, name='wav'):
 
-	##  Pklファイルなし → wav読み込み + Pklファイル作成
-    ## -------------------------------------------------
-    if not os.access(args.clean_pkl_path + '/clean.pkl', os.F_OK):
+        # Error
+        num = len(filename)
+        if num == 0:
+            print('Dataset Error : no wave files.')
 
-        ##  Wav ファイルの読み込み
-	    # wavファイルパスの獲得
-        cname = glob.glob(args.clean_wav_path + '/*.wav')
-        nname = glob.glob(args.noisy_wav_path + '/*.wav')
-        l = len(cname)  # ファイル数
-
-        # Clean wav の読み込み
         i = 1
-        cdata = []
-        for cname_ in cname:
-            cfile = wave.open(cname_, 'rb')
-            cdata.append(np.frombuffer(cfile.readframes(-1), dtype='int16'))
-            cfile.close()
-
-            print(' Load Clean wav... #%d / %d' % (i, l))
+        filedata = []
+        for filename_ in filename:
+            file_ = wave.open(filename_, 'rb')
+            filedata.append(np.frombuffer(file_.readframes(-1), dtype='int16'))
+            file_.close()
+            print(' Loading {0} wav... #{1} / {2}'.format(name, i, num))
             i+=1
 
-        cdata = np.concatenate(cdata, axis=0)               # データのシリアライズ
-        cdata = cdata - preemph * np.roll(cdata, 1)         # プリエンファシス
-        cdata = cdata.astype(np.float32)                    # データ量圧縮(メモリに余裕があるなら消す)
-        L = args.len // 2                                   # フレーム長の半分(8192サンプル)
-        D = len(cdata) // L                                 # 0.5s(8192サンプル)毎に分割
-        cdata = cdata[:D * L].reshape(D, L)                 # (1,:) --> (D, 8192)
+        filedata = np.concatenate(filedata, axis=0)             # Serializing
+        filedata = filedata - preemph * np.roll(filedata, 1)    # Pre-enphasis
+        filedata = filedata.astype(np.float32)                  # Data Compressing (float64 -> float32)
+        L = length // 2                                         # Half of Input Size (init: 8192 samples)
+        D = len(filedata) // L                                  # No. of 0.5s blocks
+        filedata = filedata[:D * L].reshape(D, L)               # Split data for each half of input size : (1,:) --> (D, 8192)
+        return filedata
 
-        print(' Clean wav is Loaded !!')
 
-        # Noisy wav の読み込み
-        i = 1
-        ndata = []
-        for nname_ in nname:
-            nfile = wave.open(nname_, 'rb')
-            ndata.append(np.frombuffer(nfile.readframes(-1), dtype='int16'))
-            nfile.close()
+	# Load settings
+    args = settings()
 
-            print(' Load Noisy wav... #%d / %d' % (i, l))
-            i += 1
+    # Make folder
+    if not os.path.exists(args.model_save_path):    # Folder of model
+        os.makedirs(args.model_save_path)
 
-        ndata = np.concatenate(ndata, axis=0)               # データのシリアライズ化
-        ndata = ndata - preemph * np.roll(ndata, 1)         # プリエンファシス
-        ndata = ndata.astype(np.float32)                    # データ量圧縮(メモリに余裕があるなら消す)
-        L = args.len // 2                                   # フレーム長の半分(8192サンプル)
-        D = len(ndata) // L                                 # 0.5s(8192サンプル)毎に分割
-        ndata = ndata[:D * L].reshape(D, L)                 # (1,:) --> (D, 8192)
+    if not os.path.exists(args.train_pkl_path):     # Folder of train pkl
+        os.makedirs(args.train_pkl_path)
 
-        print(' Now Creating Pkl file...')
+    if not os.path.exists(args.test_pkl_path):      # Folder of test pkl
+        os.makedirs(args.test_pkl_path)
 
-        ##  Pklファイルの作成
-		# クリーンpklの作成
-        with open(args.clean_pkl_path + '/clean.pkl', 'wb') as f:
+    # File name
+    if not test:
+        wav_clean   = args.clean_train_path + '/*.wav'
+        wav_noisy   = args.noisy_train_path + '/*.wav'
+        pkl_clean   = args.train_pkl_path + '/' + args.train_pkl_clean
+        pkl_noisy   = args.train_pkl_path + '/' + args.train_pkl_noisy
+    else:
+        wav_clean   = args.clean_test_path + '/*.wav'
+        wav_noisy   = args.noisy_test_path + '/*.wav'
+        pkl_clean   = args.test_pkl_path + '/' + args.test_pkl_clean
+        pkl_noisy   = args.test_pkl_path + '/' + args.test_pkl_noisy
+
+
+    ##   No pkl files -> read wav + create pkl files
+    ## -------------------------------------------------
+    if not (os.access(pkl_clean, os.F_OK) and os.access(pkl_noisy, os.F_OK)):
+
+        ##  Wav files
+        print(' Load wav file...')
+
+	    # Get file path
+        cname = glob.glob(wav_clean)
+        nname = glob.glob(wav_noisy)
+
+        # Get wave data
+        cdata = wavloader(cname, args.len, name='clean')  # Clean wav
+        ndata = wavloader(nname, args.len, name='noisy')  # Noisy wav
+
+        ##  Pkl files
+        print(' Create Pkl file...')
+
+		# Create clean pkl file
+        with open(pkl_clean, 'wb') as f:
             joblib.dump(cdata, f, protocol=-1,compress=3)
 
-        # ノイズpklの作成
-        with open(args.noisy_pkl_path + '/noisy.pkl', 'wb') as f:
+        # Create noisy pkl file
+        with open(pkl_noisy, 'wb') as f:
             joblib.dump(ndata, f, protocol=-1,compress=3)
 
-        print(' Pkl file is Created !!')
-
-	##  Pklファイルあり → ロード
+	##  Pkl files exist -> Load
     ## -------------------------------------------------
     else:
-        # クリーン音声Pklのロード
-        print(' Loading Clean wav...')
-        with open(args.clean_pkl_path + '/clean.pkl', 'rb') as f:
+        # Load clean pkl file
+        print(' Load Clean Pkl...')
+        with open(pkl_clean, 'rb') as f:
             cdata = joblib.load(f)
 
-        # ノイジィ音声Pklのロード
-        print(' Loading Noisy wav...')
-        with open(args.noisy_pkl_path + '/noisy.pkl', 'rb') as f:
+        # Load noisy pkl file
+        print(' Load Noisy Pkl...')
+        with open(pkl_noisy, 'rb') as f:
             ndata = joblib.load(f)
 
     return cdata, ndata
@@ -114,60 +129,64 @@ def data_loader(preemph=0.95):
 
 class create_batch:
     """
-    Creating Batch Data
+    Creating Batch Data for training
     """
 
-    ## 	パラメータおよびデータの保持
+    ## 	Initialization
     def __init__(self, clean_data, noisy_data, batches):
-        print(' Create batch object from waveform...')
 
-        # 正規化
+        # Normalization
         def normalize(data):
             return (1. / 32767.) * data  # [-32768 ~ 32768] -> [-1 ~ 1]
 
-        # データの整形
+        # Data Shaping
         self.clean = np.expand_dims(normalize(clean_data),axis=1)     # (D,8192,1) -> (D,1,8192)
         self.noisy = np.expand_dims(normalize(noisy_data),axis=1)     # (D,8192,1) -> (D,1,8192)
 
-        # ランダムインデックス生成 (データ取り出し用)
+        # Random index ( for data scrambling)
         ind = np.array(range(len(clean_data)-1))
         rd.shuffle(ind)
 
-        # パラメータの読み込み
+        # Parameters
         self.batch = batches
-        self.batch_num = math.ceil(len(clean_data)/batches)         # 1エポックあたりの学習数
-        self.rnd = np.r_[ind,ind[:self.batch_num*batches-len(clean_data)+1]] # 足りない分は巻き戻して利用
-        self.len = len(clean_data)                                  # データ長
-        self.index = 0                                              # 読み込み位置
+        self.batch_num = math.ceil(len(clean_data)/batches)         # Batch num for each 1 Epoch
+        self.rnd = np.r_[ind,ind[:self.batch_num*batches-len(clean_data)+1]] # Reuse beggining of data when not enough data
+        self.len = len(clean_data)                                  # Data length
+        self.index = 0                                              # Start Position for data loading
 
-    ## 	データの取り出し
+    ## 	Pop batch data
     def next(self, i):
 
-        # データのインデックス指定
-        # 各バッチではじめの8192サンプル分のインデックス
+        # Index of extracting data
         index = self.rnd[ i * self.batch : (i + 1) * self.batch ]
 
-        # データ取り出し
+        # Reconstructing clean & noisy batch : (*, 1,8192) -> (*, 1,16384)
         return np.concatenate((self.clean[index],self.clean[index+1]),axis=2), \
                np.concatenate((self.noisy[index],self.noisy[index+1]),axis=2)
 
 
 class create_batch_test:
     """
-    Creating Batch Data
+    Creating Batch Data for test
     """
 
-    ## 	パラメータおよびデータの保持
-    def __init__(self, clean_data, noisy_data, start_time = 0, stop_time=20):
-        print(' Create batch object from waveform...')
+    ## 	Initialization
+    def __init__(self, clean_data, noisy_data, start_frame=None, stop_frame=None):
 
         def normalize(data):
             return (1. / 32767.) * data  # [-32768 ~ 32768] -> [-1 ~ 1]
 
-        # データを設定
-        f_len = clean_data.shape[1] * 2     # 1データ長: 8192*2 = 16384
-        self.clean = np.expand_dims(normalize(clean_data[start_time:stop_time]).reshape(-1, f_len), axis=1)
-        self.noisy = np.expand_dims(normalize(noisy_data[start_time:stop_time]).reshape(-1, f_len), axis=1)
+        # Processing range
+        if start_frame is None:             # Start frame position
+            start_frame  = 0
+        if stop_frame is None:              # Stop frame position
+            stop_frame   = clean_data.shape[0]
+
+        # Parameters
+        f_len = clean_data.shape[1] * 2     # Inuput size : 8192*2 = 16384
+        stop_frame = 2 * math.floor((stop_frame-start_frame)/2) # Truncate protruded frame
+        self.clean = np.expand_dims(normalize(clean_data[start_frame:stop_frame]).reshape(-1, f_len), axis=1)
+        self.noisy = np.expand_dims(normalize(noisy_data[start_frame:stop_frame]).reshape(-1, f_len), axis=1)
         self.len = len(clean_data)
 
 
@@ -176,12 +195,12 @@ def wav_write(filename, x, fs=16000):
     # x = de_emph(x)      # De-emphasis using LPF
 
     x = x * 32767       # denormalized
-    x = x.astype('int16')               # cast to int
+    x = x.astype('int16')  # cast to int
     w = wave.Wave_write(filename)
     w.setparams((1,     # channel
                  2,     # byte width
                  fs,    # sampling rate
-                 len(x),  # number of frames
+                 len(x),  # #. of frames
                  'NONE',
                  'not compressed' # no compression
     ))
