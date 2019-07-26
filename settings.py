636e85c73669fa7dcf30e5265e5d16607f9a0310
YosukeSUGIURA <44398746+YosukeSugiura@users.noreply.github.com>
2019-07-26 20:02:08 +0900

    Some revision 2019.7.26

--- settings.py
+++ settings.py
@@ -15,20 +15,16 @@ class settings:
         # Parameters
         self.batch_size 	    = 150               # Batch size
         self.epoch              = 100               # Epoch
-        self.learning_rate_pre_dae = 0.001         # Learning Rate (Denoising Auto Encoder)
         self.learning_rate_gen  = 0.0001            # Learning Rate (Generator)
         self.learning_rate_dis  = 0.000001          # Learning Rate (Discriminator)
         self.weight_decay_rate 	= 0.05              # Regularization rate (weight decay = learning_rate * weight_decay_rate)
-        self.alpha              = 0.0001
-        self.lp                 = 1.0
 
         # Retrain
         self.epoch_from         = 0                 # Epoch No. from that Retraining starts (init:0)
 
         # Save path
-        self.model_save_path    = 'params_prop'          # Network model path
+        self.model_save_path    = 'params'          # Network model path
         self.model_save_cycle   = 2                      # Epoch cycle for saving model (init:1)
-        self.result_save_path   = 'monitor'              # Network model path
 
         # Save wav path
         self.wav_save_path      = 'pred'
@@ -48,7 +44,3 @@ class settings:
         self.test_pkl_path      = 'pkl'             # Folder of pkl files for test
         self.test_pkl_clean     = 'test_clean.pkl'  # File name of "Clean" pkl for test
         self.test_pkl_noisy     = 'test_noisy.pkl'  # File name of "Noisy" pkl for test
-
-
-        # additional
-        self.learning_rate_msk = 0.00005  # Learning Rate
